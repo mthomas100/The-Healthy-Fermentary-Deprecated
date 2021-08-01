0d6a29f162052f363f04cdfde7ef80ffca56ebf0
Matthew Thomas <matthewthomas@Matthews-MacBook-Pro.local>
2021-08-01 14:34:21 -0400

    preventing FOUT by updated styled/material enabled document sheet

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 import { ServerStyleSheets } from '@material-ui/styles';
-// import theme from '../src/theme';
 import { theme } from '../components/styles/materialStyles';
 
 class MyDocument extends Document {
@@ -40,13 +39,12 @@ class MyDocument extends Document {
       <Html lang="en" dir="ltr">
         <Head>
           <meta charSet="utf-8" />
-          {/* Use minimum-scale=1 to enable GPU rasterization */}
-          <meta
-            name="viewport"
-            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
-          />
           {/* PWA primary color */}
-          {/* <meta name="theme-color" content={theme.palette.primary.main} /> */}
+          <meta name="theme-color" content={theme.palette.primary.main} />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+          />
           <link
             href="https://fonts.googleapis.com/css2?family=Reenie+Beanie&display=swap"
             rel="stylesheet"
