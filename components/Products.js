0cbb8c05986372ea4e9e9e9be371f08e40845c79
Matthew Thomas <matthewthomas@Matthews-MacBook-Pro.local>
2021-08-30 16:04:01 -0400

    products

--- components/Products.js
+++ components/Products.js
@@ -30,7 +30,6 @@ export default function Products({ products }) {
   const { setCartIsHovering } = useCart();
   const [contentSizeArray, setContentSizeArray] = useState([]);
   const [maxContentHeight, setMaxContentHeight] = useState(undefined);
-  const windowSize = useWindowSize();
 
   useLayoutEffect(() => {
     setMaxContentHeight(Math.max(...contentSizeArray));
