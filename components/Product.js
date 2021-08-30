0e341cb72e6cbb8ebbe782392326411a829b4b64
Matthew Thomas <matthewthomas@Matthews-MacBook-Pro.local>
2021-08-30 15:52:05 -0400

    implemented prices (hardcode v1)

--- components/Product.js
+++ components/Product.js
@@ -1,45 +1,39 @@
 import React, { useLayoutEffect, useEffect, useRef, useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-// import { CardContent as CardContentMUI } from '@material-ui/core/CardContent';
-import {
-  Typography as TypographyMUI,
-  CardContent as CardContentMUI,
-  Card as CardMUI,
-  CardActionArea,
-} from '@material-ui/core';
 import Image from 'next/image';
 import styled from 'styled-components';
 import AddToCart from './AddToCart';
 import useComponentSize from '../lib/useComponentSize';
 import { useWindowSize } from '../lib/useWindowSize';
+import {
+  Card,
+  CardContent,
+  ProductStyles,
+  TypographyPrimary,
+  TypographySecondary,
+} from './styles/ProductStyles';
 
-const TypographySecondary = styled(TypographyMUI)`
-  && {
-    line-height: 2;
+const PriceStyles = styled.div`
+  position: absolute;
+  z-index: 20;
+  top: 5%;
+  right: 5%;
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  background-color: #000000c5;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .currency {
+    margin-right: 2px;
+  }
+
+  .price {
+    text-align: center;
+    color: white;
     font-size: 1.3rem;
   }
-`;
-
-const TypographyPrimary = styled(TypographyMUI)``;
-
-const Card = styled(CardMUI)`
-  && {
-    border-bottom-left-radius: 0;
-    border-bottom-right-radius: 0;
-  }
-`;
-
-const CardContent = styled(CardContentMUI)`
-  && {
-    min-height: ${(props) => props.maxContentHeight}px;
-  }
-`;
-
-const ProductStyles = styled.div`
-  position: relative;
-  min-width: 30rem;
-  width: 30rem;
-  padding-bottom: ${(props) => props.addToCartSize?.height}px;
 `;
 
 export default function Product({
@@ -59,6 +53,12 @@ export default function Product({
 
   return (
     <ProductStyles addToCartSize={addToCartSize} ref={productRef}>
+      <PriceStyles>
+        <div className="price">
+          <span className="currency">₹</span>
+          {product.price}
+        </div>
+      </PriceStyles>
       <Card>
         <Image
           src={product.image.url}
---
+++ components/styles/ProductStyles.js
@@ -0,0 +1,42 @@
+import {
+  Typography as TypographyMUI,
+  CardContent as CardContentMUI,
+  Card as CardMUI,
+} from '@material-ui/core';
+import styled from 'styled-components';
+
+const ProductStyles = styled.div`
+  position: relative;
+  min-width: 30rem;
+  width: 30rem;
+  padding-bottom: ${(props) => props.addToCartSize?.height}px;
+`;
+
+const TypographyPrimary = styled(TypographyMUI)``;
+const TypographySecondary = styled(TypographyMUI)`
+  && {
+    line-height: 2;
+    font-size: 1.3rem;
+  }
+`;
+
+const Card = styled(CardMUI)`
+  && {
+    border-bottom-left-radius: 0;
+    border-bottom-right-radius: 0;
+  }
+`;
+
+const CardContent = styled(CardContentMUI)`
+  && {
+    min-height: ${(props) => props.maxContentHeight}px;
+  }
+`;
+
+export {
+  ProductStyles,
+  TypographyPrimary,
+  TypographySecondary,
+  Card,
+  CardContent,
+};
