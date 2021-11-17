41d62dcf6c7770448d460c69b9fd67e057dfa597
Matthew Thomas <matthewthomas@Matthews-MacBook-Pro.local>
2021-11-17 11:09:19 -0500

    fully responsive hero component

--- components/Hero.js
+++ components/Hero.js
@@ -1,70 +1,61 @@
 import styled from 'styled-components';
 import Image from 'next/image';
 import ReactPlayer from 'react-player';
-import { useWindowSize } from 'react-use';
-import { useEffect } from 'react';
+import { useWindowSize, useMeasure } from 'react-use';
 import useBreakpoint from '../lib/useBreakpoint';
 
 export default function Hero() {
-  const { width } = useWindowSize();
-  const breakpoint = useBreakpoint();
+  const [headlineRef, { width: headlineWidth }] = useMeasure();
 
-  console.log(breakpoint);
   return (
-    <div className="relative w-full h-auto ">
+    <div className="relative py-32">
       <Image
         src="/images/kombuchaFruit.jpg"
         alt="Kombucha"
-        layout="responsive"
-        width="100%"
-        height={
-          {
-            xxs: '150%',
-            xs: '50%',
-            sm: '50%',
-            md: '50%',
-            lg: '50%',
-            xl: '50%',
-          }[breakpoint]
-        }
+        layout="fill"
         objectFit="cover"
         className="absolute inset-0 z-0 backdrop-filter backdrop-grayscale-0"
       />
-      <div className="z-10 absolute inset-0 bg-gray-500 opacity-50" />
-
       <div
-        id="heading"
-        className="absolute z-20 inset-0 flex flex-col justify-center items-center text-left font-bold tracking-wide gap-y-4 xs:gap-y-7 sm:gap-y-10 text-3xl xxs:text-4xl xs:text-5xl sm:text-6xl lg:text-8xl"
-      >
-        <div className="text-white">
-          <span>Fermented Beverages</span>
-        </div>
-        <div>
-          <span className="bg-white text-black p-2 sm:p-4">
-            Delivered to your Doorstep
-          </span>
-        </div>
+        id="imageFilter"
+        className="z-10 absolute inset-0 bg-gray-500 opacity-50"
+      />
+      <div className="relative w-full min-h-full ">
+        <div
+          id="textAndVideo"
+          className="relative z-20 inset-0 flex flex-col justify-center items-center text-left font-bold tracking-wide gap-y-7 sm:gap-y-10 lg:gap-y-16 text-3xl xxs:text-4xl xs:text-5xl sm:text-6xl lg:text-8xl"
+        >
+          <div className="text-white">
+            <span>Fermented Beverages</span>
+          </div>
+          <div ref={headlineRef}>
+            <span className="bg-white text-black p-2 sm:p-4">
+              Delivered to your Doorstep
+            </span>
+          </div>
 
-        <ReactPlayer
-          className="mx-4"
-          url="https://vimeo.com/182458845"
-          width="100%"
-          // height="auto"
-          config={{
-            file: {
-              attributes: {
-                controlsList: 'nodownload',
+          <ReactPlayer
+            className="mt-16"
+            url="https://vimeo.com/182458845"
+            width={headlineWidth}
+            height={headlineWidth * 0.5625}
+            // height="auto"
+            config={{
+              file: {
+                attributes: {
+                  controlsList: 'nodownload',
+                },
               },
-            },
-            vimeo: {
-              playerOptions: {
-                autopause: false,
-                muted: true,
-                playsinline: true,
+              vimeo: {
+                playerOptions: {
+                  autopause: false,
+                  muted: true,
+                  playsinline: true,
+                },
               },
-            },
-          }}
-        />
+            }}
+          />
+        </div>
       </div>
     </div>
   );
