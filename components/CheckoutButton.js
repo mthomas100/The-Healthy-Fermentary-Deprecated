75ef2277678d48ad2383c677a614be6eb03f08a5
Matthew Thomas <matthewthomas@Matthews-MacBook-Pro.local>
2021-12-05 12:43:20 -0500

    set cart hover state to false upon hitting the  on desktop

--- components/CheckoutButton.js
+++ components/CheckoutButton.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import Link from 'next/link';
 import { Button as ButtonMUI } from '@material-ui/core';
 import { useRef } from 'react';
+import { useCart } from '../lib/cartState';
 
 const Button = styled(ButtonMUI)`
   && {
@@ -14,11 +15,16 @@ const CheckoutButtonStyles = styled.div``;
 
 export default function CheckoutButton() {
   const checkoutButtonRef = useRef(null);
+  const { setCartIsHovering } = useCart();
 
   return (
     <CheckoutButtonStyles ref={checkoutButtonRef}>
       <Link href="/checkout" passHref>
-        <Button variant="outlined" size="large">
+        <Button
+          variant="outlined"
+          size="large"
+          onClick={() => setCartIsHovering(false)}
+        >
           Checkout
         </Button>
       </Link>
